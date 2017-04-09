9e9ef3d1d3266623de9e62c03595a3507497595d
吕杰 <991102@nd.com>
2017-04-09 18:01:22 +0800

    第六章  XML绘图

--- app/src/main/java/com/example/androidqunyinhui/chapter/six/ChapterSixMainActivity.java
+++ app/src/main/java/com/example/androidqunyinhui/chapter/six/ChapterSixMainActivity.java
@@ -4,10 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.androidqunyinhui.R;
 
 public class ChapterSixMainActivity extends AppCompatActivity {
+
+    private TextView textView2;
 
     public static void startActivity(Context context){
         Intent intent = new Intent(context, ChapterSixMainActivity.class);
@@ -18,5 +22,18 @@ public class ChapterSixMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chapter_six_main);
+
+        initView();
     }
+
+    private void initView(){
+        textView2 = (TextView) findViewById(R.id.tv_2);
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
 }
