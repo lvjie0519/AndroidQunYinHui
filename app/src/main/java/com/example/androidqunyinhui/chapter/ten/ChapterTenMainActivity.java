5509ba1b5c4d7cafb5938204d6688560e5a030e3
吕杰 <991102@nd.com>
2017-04-17 19:37:55 +0800

    增加获取app内存大小，完成第十章

--- app/src/main/java/com/example/androidqunyinhui/chapter/ten/ChapterTenMainActivity.java
+++ app/src/main/java/com/example/androidqunyinhui/chapter/ten/ChapterTenMainActivity.java
@@ -1,9 +1,12 @@
 package com.example.androidqunyinhui.chapter.ten;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.example.androidqunyinhui.R;
 
@@ -18,5 +21,23 @@ public class ChapterTenMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chapter_ten_main);
+
+        initView();
     }
+
+    private void initView(){
+        findViewById(R.id.btn_1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+                int largeHeapSize = activityManager.getLargeMemoryClass();
+                int heapSize = activityManager.getMemoryClass();
+                Log.i("lvjie","最大内存："+largeHeapSize+"  内存："+heapSize);
+
+            }
+        });
+    }
+
+
 }
