1bd69db6f8daeba9ab9add817887ffbe11210854
lvjie <991102@nd.com>
2018-06-29 14:40:06 +0800

    补间动画  set集合

--- app/src/main/java/com/example/androidqunyinhui/android/donghua/TweenAnimationDemoActivity.java
+++ app/src/main/java/com/example/androidqunyinhui/android/donghua/TweenAnimationDemoActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.androidqunyinhui.R;
 
@@ -16,6 +17,7 @@ import com.example.androidqunyinhui.R;
 public class TweenAnimationDemoActivity extends AppCompatActivity {
 
     private ImageView imageView;
+    private TextView textView;
 
     public static void startActivity(Context context){
         Intent intent = new Intent(context, TweenAnimationDemoActivity.class);
@@ -32,9 +34,15 @@ public class TweenAnimationDemoActivity extends AppCompatActivity {
 
     private void initView(){
         imageView = (ImageView) findViewById(R.id.iv_1);
+        textView = (TextView) findViewById(R.id.tv_zuhe);
 
         Animation anim = AnimationUtils.loadAnimation(this, R.anim.tween_animation_alpha);
         imageView.startAnimation(anim);
+
+        // 步骤1:创建 需要设置动画的 视图View
+        Animation translateAnimation = AnimationUtils.loadAnimation(this, R.anim.setlist_animation_alpha);
+        // 步骤2:创建 动画对象 并传入设置的动画效果xml文件
+        textView.startAnimation(translateAnimation);
     }
 
 }
