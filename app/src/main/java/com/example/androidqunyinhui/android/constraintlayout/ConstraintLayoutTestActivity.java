64180b9d5f90e031a464074b3e0586778b2f378b
lvjie <991102@nd.com>
2018-08-12 15:45:32 +0800

    ConstaraintLayout

---
+++ app/src/main/java/com/example/androidqunyinhui/android/constraintlayout/ConstraintLayoutTestActivity.java
@@ -0,0 +1,15 @@
+package com.example.androidqunyinhui.android.constraintlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.androidqunyinhui.R;
+
+public class ConstraintLayoutTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout_test);
+    }
+}
